f97e7785c5383801f6dd0c74c110c4348302df25
Vizx Enterprises <developers@vizter.xyz>
2020-06-13 12:43:12 +0530

    Added Config.js To run on config

---
+++ config.js
@@ -0,0 +1,8 @@
+//Uses WalletApi
+module.exports.config = {
+  host : "185.8.177.12", //The Ip Address or Domain Where WalletApi is Hosted
+  port : 9999, //The Port where WalletApi is Running
+  password : "", //The Password Of The WalletApi
+  daemonHost : "185.8.177.12", //The Ip Address or Domain Where Daemon Of The Coin is Hosted,
+  daemonPort : 3997 //The Port Of The Daemon
+}
